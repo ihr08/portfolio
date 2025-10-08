b3c87d7282b9aec1e845a2c2c9ad31c0808a6733
ihr08 <i.hrr.in@gmail.com>
2025-10-08 23:17:59 +0530

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -19,12 +19,12 @@ export const ThemeToggle = () => {
   const toggleTheme = () => {
     if (isDarkMode) {
       document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "dark");
-      setIsDarkMode(true);
-    } else {
-      document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "light");
       setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
     }
   };
 
@@ -37,9 +37,9 @@ export const ThemeToggle = () => {
       )}
     >
       {isDarkMode ? (
-        <Moon className="h-6 w-6 text-blue-900" />
+        <Sun className="h-6 w-6 text-yellow-300" />
       ) : (
-        <sun className="h-6 w-6 text-yellow-300" />
+        <Moon className="h-6 w-6 text-blue-900" />
       )}
     </button>
   );
